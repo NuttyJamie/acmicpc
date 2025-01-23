bd3600fbc187a4a418ad95d3201fd17e72cfeff5
NuttyJamie <jongmie91@gmail.com>
2025-01-24 00:17:40 +0900

    Not solved yet

--- 2447.c
+++ 2447.c
@@ -1,13 +1,43 @@
 #include <stdio.h>
 
-char square(int n)
+int draw_border(int n)
 {
+	if(n == 3)	putchar('*');
+	else		square(n/3, 0);
+}
+
+int draw_centre(int n)
+{
+	if(n == 3)	putchar(' ');
+	else 		putchar(' ');
+}
+
+int square(int n, int flag)
+{
+	draw_border(n);
+	draw_border(n);
+	draw_border(n);
+	if(flag)	putchar('\n');
+	draw_border(n);
+	draw_centre(n);
+	draw_border(n);
+	if(flag)	putchar('\n');
+	draw_border(n);
+	draw_border(n);
+	draw_border(n);
+}
+
+int main()
+{
+	int n;
+
+	scanf("%d", &n);
+	square(n, 1);
+}
+
 	//square(27)
 	//square(9)+square(9)+..
 	//square(3)+square(3)+..
-	//
-	//square
-	//
 	//*********
 	//* ** ** *
 	//*********
@@ -17,7 +47,3 @@ char square(int n)
 	//*********
 	//* ** ** *
 	//*********	
-	int i;
-
-	for(i=n; ;  
-}
